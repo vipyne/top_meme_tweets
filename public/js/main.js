2d6ba5db8880860bfb95017226d8661128c9834d
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-12-12 11:11:54 -0800

    Use Handlebars.js for templating.
    Replace inline home page with one from a template.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
-$(function() {
 
+function renderTemplate(name, context, callback) {
+  function compileAndLoadTemplate(source) {
+    var template = Handlebars.compile(source)
+    var html = template(context)
+    callback(html)
+  }
+
+  $.get("/templates/"+name+".hbs", compileAndLoadTemplate)
+}
+
+$(function() {
+  renderTemplate("home", undefined, function(html) {
+    $(".container").html(html)  
+  })
 })
