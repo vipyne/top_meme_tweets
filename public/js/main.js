022fac259ccdd5dfb77788e9a12bfad421216657
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-12-13 00:37:53 -0800

    WIP: THIS CODE HAS INTENTIONAL BUGS

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,66 @@
+var TEMPLATES = (function() {
+  var _cache = {}
 
-function renderTemplate(name, context, callback) {
-  function compileAndLoadTemplate(source) {
-    var template = Handlebars.compile(source)
-    var html = template(context)
-    callback(html)
+  function _tUrl(name) {
+    return "/templates/" + name + ".hbs"
   }
 
-  $.get("/templates/"+name+".hbs", compileAndLoadTemplate)
+  return {
+    precompile: function(names) {
+      for (var i in names) {
+        $.get(_tUrl(names[i]), function(source) {
+          this.compileAndCache(names[i], source)
+        })
+      }
+    },
+
+    compileAndCache: function(name, source) {
+      var template = Handlebars.compile(source)
+      _cache[name] = template
+      return template
+    },
+
+    render: function(name, context, renderCallback) {
+      var self = this
+      function compileCacheAndRender(source) {
+        template = self.compileAndCache(name, source)
+        renderCallback(template(context))
+      }
+
+      var template = _cache[name]
+      if (template) {
+        renderCallback(template(context))
+      } else {
+        $.get(_tUrl(name, compileCacheAndRender)
+      }
+    }
+  }
+}())
+
+var Controller = {
+  init: function() {
+    TEMPLATES.precompile(['tweet'])
+    $('form').on('submit', this.showTweetsForMeme)
+  },
+
+  showTweetsForMeme: function(e) {
+    e.preventDefault()
+    $(".container .tweets").html('')
+    var $form = $(event.target)
+    $.post($form.attr('action'), $form.serialize(), this.renderTweets.bind(this))
+  },
+
+  renderTweets: function(tweets) {
+    for (var i in tweets) {
+      TEMPLATES.render('tweet', tweets[i], this.appendTweet)
+    }}
+  },
+
+  appendTweet: function(html) {
+    $(".container .tweets").append(html)
+  }
 }
 
 $(function() {
-  renderTemplate("home", undefined, function(html) {
-    $(".container").html(html)  
-  })
-
-  $.post('/mock-tweets-for-meme', "meme=addawordruinamovie")
-  .done(function(resp) {
-    console.log(resp)
-  })
-
+  Controller.init()
 })
